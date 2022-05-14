6a46ca4168153b3ebd3c24ae65a662ce6cd4cf34
OrEliyahu43 <eurekas300@gmail.com>
2022-05-14 17:07:39 +0300

    next

---
+++ script/script.js
@@ -0,0 +1,73 @@
+
+const covidByCountry = {
+    'others': {
+        'names': [],
+        'data': []
+    },
+    Americas: {
+        'names': [],
+        'data': []
+    },
+    Europe: {
+        'names': [],
+        'data': []
+    },
+    Africa:{
+        'names': [],
+        'data': []
+    },
+    Asia: {
+        'names': [],
+        'data': []
+    },
+    Oceania: {
+        'names': [],
+        'data': []
+    }
+};
+
+
+async function orderCoutries(url) {
+    const continents = {};
+
+    const allCountryData = await fetch(url);
+    const allCounties = await allCountryData.json()
+    for (country of allCounties) {
+
+        const name = country.name.common;
+        const regionName = country.region;
+        if (regionName === '')
+            continents[name] = 'others';
+        else {
+            continents[name] = regionName;
+        }
+
+    }
+    return continents;
+}
+
+
+async function getCovidByCountry() {
+
+    const countriesByRegion = await orderCoutries(`https://restcountries.herokuapp.com/api/v1`)
+    const covid_19_Data = await fetch(` https://corona-api.com/countries`);
+    const covid19Parsed = await covid_19_Data.json();
+    for (country of covid19Parsed.data) {
+
+        const countryName = country.name;
+        const continent = countriesByRegion[countryName]
+        if (continent === undefined) {
+            continue;
+        }
+
+        const covidDataCountry = country['latest_data'];
+        covidByCountry[continent]['names'].push(countryName);
+        covidByCountry[continent]['data'].push(covidDataCountry)
+ 
+   
+    }
+}
+
+getCovidByCountry().then(() => {
+    console.log(covidByCountry);
+})
